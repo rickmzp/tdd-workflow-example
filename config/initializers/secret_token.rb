f8e396643a47011c285bbd69029b64c497875abe
Rick Martínez <rick@codecademy.com>
2013-06-19 12:59:52 -0400

    dependency setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TddWorkflowExample::Application.config.secret_token = '81d7879349a1df94a0f8691e5b7b2da208543cdde056ae5e48ab11e92ecd8d3f80537ae6279cb4d403f1dfcba22684f61a877839cfd0cab167862b2acb0cfd2a'
